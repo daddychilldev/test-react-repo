d4234ab062eef0b792e9230ad44b94ffc7987b8c
Devcanz <devcanz17@gmail.com>
2025-06-18 08:20:23 +0800

    add-on content

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -69,6 +69,132 @@ const Content = ({ buttonText = "Give Me This Buy 1, Get 1 FREE Deal Before It's
         <div className="yotpo">
           <YotpoReview />
         </div>
+        <div className="btndiv pulse">
+          <button className="btn">{buttonText}</button>
+        </div>
+        <div className="subheader">
+          <span className="subheadertext orange">
+          Seriously, This Holster Works With 99.9% of Single Semi-Automatic Pistols On the Planet
+          </span>
+        </div>
+        <div className="holster3">
+          <img 
+            src="https://cdn.shopify.com/s/files/1/0670/4948/8684/files/BlackHolsterDesktop1.webp?v=1729102182" 
+            alt="heroimage" 
+            className="hero-image" 
+          />
+        </div>
+        <article className="bodymsg">
+          <p>Our holster design means that <b>regardless of what pistol you own</b> it will help you safely and comfortably carry it.</p>
+          <p>No more needing to buy multiple holsters for all your pistols.</p>
+          <p>Plus, since it has 2-built in mag pouches, now <b>you don't need to spend extra money on mag pouches</b> to guarantee you’re never out of the fight.</p>
+          <p>Take a look at the list of brands our holster works with and then grab yours before the price goes up!</p>
+        </article>
+        <div>
+          <img 
+            src="https://cdn.shopify.com/s/files/1/0670/4948/8684/files/LogoDesktop.webp?v=1729112354" 
+            alt="heroimage" 
+            className="hero-image" 
+          />
+        </div>
+        <div className="btndiv pulse">
+          <button className="btn">{buttonText}</button>
+        </div>
+        <div className="subheader">
+          <span className="subheadertext orange">
+          Don’t Let An Uncomfortable Holster Be The Reason You’re Unarmed When Bad Guys Attack
+          </span>
+        </div>
+        <article className="bodymsg">
+          <p>The vast majority of gun owners don’t carry their gun daily for 1 simple reason.</p>
+          <p>They don’t like how their holster feels.</p>
+          <p>The good news is that the VNSH Holster is made for all day wear… and owning <b>it is the #1 thing you can do to ensure you're always ready to defend yourself</b> and your family.</p>
+          <p>The VNSH holster is superior to leather and kydex.</p>
+          <p>Where kydex can be rigid and uncomfortable… and leather is sweaty and sticky, the VNSH holster is ultra-comfortable because it uses a custom blend of sweat-wicking fabrics that we call "yoga pant" fabric to help you stay cool and dry in the nastiest environments.</p>
+          <p>Not to mention the waist band is made from a crazy comfy velcro <b>that will not</b> snag on shirts, waistbands and the like.The VNSH holster is superior to leather and kydex.</p>
+          <p>And because of its unique no-clip, no hook design, <b>you can comfortably configure the holster any which way you want.</b></p>
+          <p>Want to carry at 3 o’clock? Go for it.</p>
+          <p>Like pure appendix carry? That’s easy.</p>
+          <p>Does carrying in the small of your back suit you best? Well, it works there too.</p>
+          <p>You can even carry it up high on your chest or side thanks to the adjustable strap (which extends up to 48 inches and 68 inches with our extension strap).</p>
+          <p>Also, we combined that awesome "yoga pant" fabric with rugged 1000D Cordura so it will withstand years and years of abuse without showing a sign of distress.</p>
+          <p>And best of all it features an <b>enhanced trigger guard</b>. Yes, it’s true, the VNSH holster is the only bellyband on the planet with a dedicated trigger guard built in. A solid but flexible piece of .7mm plastic resits in the custom-built holster to prevent <b>anything</b> from accidentally causing a negligent discharge.</p>
+          <p>Not to mention the retention is rock-solid… <b>but still incredibly easy to draw.</b> Never fear that your gun will fall out, or that a criminal will be able to disarm you.</p>
+        </article>
+        <div className="holster3">
+          <img 
+            src="https://cdn.shopify.com/s/files/1/0670/4948/8684/files/BlackHolsterDesktop2.webp?v=1729102182" 
+            alt="heroimage" 
+            className="hero-image" 
+          />
+        </div>
+        <div className="btndiv pulse">
+          <button className="btn">{buttonText}</button>
+        </div>
+        <div className="subheader">
+          <span className="subheadertext orange">
+          Crazy Holiday Deal! <br></br><span className="red">Buy 1 Holster, Get 1 FREE!</span>
+          </span>
+        </div>
+        <div className="pad1">
+          <img 
+            src="https://cdn.shopify.com/s/files/1/0670/4948/8684/files/BlackHolsterDesktop4.webp?v=1729102312" 
+            alt="heroimage" 
+            className="hero-image" 
+          />
+        </div>
+        <article className="bodymsg">
+          <p>As you’ve seen… people <em><b>love</b></em> the VNSH Holster.</p>
+          <p>In fact, most people who buy one almost always <b>end up wanting at least one more</b> – either for themselves or to give to a friend or family member.</p>
+          <p>So since the holidays are right around the corner…</p>
+          <p>We’re giving you the <b>biggest deal we’ve EVER offered!</b></p>
+          <p className="yellow1"><b>Buy 1 Holster, Get 1 FREE!</b></p>
+          <p>Plus, when you grab this deal today, you’ll be doing so <b>100% Risk-Free, because…</b></p>
+        </article>
+        <div className="subheader">
+          <span className="subheadertext orange">
+          You’re Getting An <span className="red">Iron-Clad,</span><br></br><span className="red">Money-Back</span> Guarantee
+          </span>
+        </div>
+        <div className="pad1">
+          <img 
+            src="https://cdn.shopify.com/s/files/1/0670/4948/8684/files/guarantee-money-back-200.webp?v=1729100261" 
+            alt="heroimage" 
+            className="hero-image" 
+          />
+        </div>
+        <article className="bodymsg">
+          <p>Love everything about your order, or <b><em>we’ll refund you every penny.</em></b></p>
+          <p>It’s that simple.</p>
+          <p>If you aren’t totally thrilled, all you have to do is contact our <b><em>US-Based Support Team</em></b> within 60 days to get a full refund.</p>
+          <p><b>Plus, we also give you a <em><span className="yellow1 font2">2-year workmanship guarantee as well!</span></em></b></p>
+          <p>We know you’ll love your VNSH gear, which is why we’re happy to extend you these iron-clad guarantees.</p>
+          <p>So if you want the <b><em>comfiest holster on earth</em></b> … plus a second one <b><em>totally FREE on the house</em></b>…</p>
+          <p>Plus… get a FREE VNSH Holster with it…</p>
+          <p>Then you owe it to yourself to grab this deal now!</p>
+        </article>
+        <div className="btndiv pulse">
+          <button className="btn">{buttonText}</button>
+        </div>
+        <div className="subheader">
+          <span className="subheadertext orange">
+          Fair Warning… <br></br>Big Deal <span className="red">Before Prices Go Up…</span>
+          </span>
+        </div>
+        <article className="bodymsg">
+          <p>Since we launched in late 2022, we’ve done everything in our power to keep the VNSH Holster <b><em>as affordable as possible.</em></b></p>
+          <p>But sadly, inflation is finally forcing us to raise prices on our holster.</p>
+          <p>Now, as much of a bummer as that is… the <b>GOOD NEWS</b> is that…</p>
+          <p>We’re running this huge <b><u><span className="yellow1">Buy 1, Get 1 FREE</span></u></b> deal to make sure everyone has a chance to get the VNSH Holster for an awesome price before it becomes more expensive in the weeks ahead.</p>
+          <p>It won’t last forever though…</p>
+          <p>Once this is over, you’ll never see a deal this good on VNSH Holsters again.</p>
+          <p>It won’t last forever though…</p>
+          <p>So don’t wait!</p>
+          <p><b>Grab your VNSH Holster and <span className="yellow1">get a second one 100% FREE</span> while you still can!</b></p>
+        </article>
+        <div className="btndiv pulse">
+          <button className="btn">{buttonText}</button>
+        </div>
       </section>
     </>
   );
